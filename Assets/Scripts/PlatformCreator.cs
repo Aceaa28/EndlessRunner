8191cca076972e7f6a92b636f3928997bc1ff9dc
Aceaa28 <ahernandez3@aoiths.org>
2023-03-15 12:29:50 -0400

    module 6

--- Assets/Scripts/PlatformCreator.cs
+++ Assets/Scripts/PlatformCreator.cs
@@ -4,22 +4,21 @@ using UnityEngine;
 
 public class PlatformCreator : MonoBehaviour
 {
-    [SerializeField] GameObject platformPrefab;
+    [SerializeField] GameObject[] platformPrefab;
     [SerializeField] Transform referencePoint;
-    private GameObject lastCreatedPlatform;
+    [SerializeField] private GameObject lastCreatedPlatform;
+    [SerializeField] float spaceBetweenPlatforms = 2;
+    float lastPlatformWidth;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-       lastCreatedPlatform = Instantiate(platformPrefab, referencePoint.position, Quaternion.identity);
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (lastCreatedPlatform.transform.position.x < 0)
+        if (lastCreatedPlatform.transform.position.x < referencePoint.position.x)
         {
-            lastCreatedPlatform = Instantiate(platformPrefab, referencePoint.position, Quaternion.identity);
+            Vector3 targetCreationPoint = new Vector3(referencePoint.position.x + lastPlatformWidth + spaceBetweenPlatforms, -3, 0);
+            int randomPlatform = Random.Range(0, 3);
+            lastCreatedPlatform = Instantiate(platformPrefab[randomPlatform], targetCreationPoint, Quaternion.identity);
+            BoxCollider2D collider = lastCreatedPlatform.GetComponent<BoxCollider2D>();
+            lastPlatformWidth = collider.bounds.size.x;
         }
     }
 }
