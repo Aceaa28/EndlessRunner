21fe952db36f39438919bfc15a16541750be62a3
Aceaa28 <ahernandez3@aoiths.org>
2023-03-19 15:33:00 -0400

    Module 7

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,12 +6,81 @@ public class Player : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
     [SerializeField] float jumpForce = 5;
+    [SerializeField] Transform raycastOrigin;
+    [SerializeField] bool isGrounded;
+    bool jump;
+    [SerializeField] Animator anim;
+    float lastYPos;
 
-    // Update is called once per frame
+    private void Start() 
+    {
+        lastYPos = transform.position.y;
+    }
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        CheckForInput();
+        CheckIfPlayerIsFalling();
+    }
+
+    void FixedUpdate()
+    {
+        CheckForGrounded();
+        CheckForJump();
+    }
+
+    void CheckForJump()
+    {
+         if (jump == true) 
+        {
+           jump = false;
+           rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
+    }
+
+    void CheckIfPlayerIsFalling()
+    {
+        if (transform.position.y < lastYPos) 
+        {
+            anim.SetBool("Falling", true);
+        }
+        else 
+        {
+            anim.SetBool("Falling", false);
+        }
+
+        lastYPos = transform.position.y;
+    }
+
+    void CheckForInput () 
+    {
+        if(isGrounded == true)
+        {
+             if (Input.GetKeyDown(KeyCode.Space)) 
+             {
+                jump = true;
+                anim.SetTrigger("Jump");
+             }
+        } 
+    }
+
+    void CheckForGrounded() 
+    {
+        RaycastHit2D hit = Physics2D.Raycast(raycastOrigin.position, Vector2.down);
+
+        if(hit.collider != null) 
+        {
+            if(hit.distance < 0.1f) 
+            {
+                isGrounded = true;
+                anim.SetBool("IsGrounded", true);
+            }            
+            Debug.DrawRay(raycastOrigin.position, Vector2.down, Color.green);
+        }
+        else 
+            {
+                isGrounded = false;
+                anim.SetBool("IsGrounded", false);
+            }
     }
 }
