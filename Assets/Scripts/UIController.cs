cc1d530f85654e6974c0896ab5971e1f2270b8c1
Aceaa28 <ahernandez3@aoiths.org>
2023-03-20 00:03:33 -0400

    Module 8

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+	[SerializeField] Text distanceTraveled;
+	[SerializeField] GameObject gameOverScreen;
+	[SerializeField] Player player;
+
+	private void Update() 
+	{
+		if(Input.GetKeyDown(KeyCode.T)) 
+		{
+			ShowGameOverScreen();
+		}
+	}
+
+	void ShowGameOverScreen()
+	{
+		gameOverScreen.SetActive(true);
+		//distanceTraveled.text = player.distanceTraveled.ToString();
+		float roundedDistance = Mathf.Ceil(player.distanceTraveled);
+		distanceTraveled.text = "" + roundedDistance;
+	}
+
+	public void GameRestart() 
+	{
+		Debug.Log("Restart the game");
+	} 
+}
